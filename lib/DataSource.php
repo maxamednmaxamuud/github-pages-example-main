3ea5107b2bd518f093cde9ecc9963efeac783b58
maxamednmaxamuud <124230978+maxamednmaxamuud@users.noreply.github.com>
2023-02-05 13:21:16 +0300

    Add files via upload

---
+++ lib/DataSource.php
@@ -0,0 +1,139 @@
+<?php
+
+namespace Phppot;
+
+/**
+ * Generic datasource class for handling DB operations.
+ * Uses MySqli and PreparedStatements.
+ *
+ * @version 2.7 - PDO connection option added
+ */
+class DataSource
+{
+
+  
+    const HOST = '0.0.0.0:3306';
+
+    const USERNAME = 'root';
+
+    const PASSWORD = 'root';
+
+    const DATABASENAME = 'Netbook';
+
+    private $conn;
+
+   
+    function __construct()
+    {
+        $this->conn = $this->getConnection();
+    }
+
+ 
+    public function getConnection()
+    {
+        $conn = new \mysqli(self::HOST, self::USERNAME, self::PASSWORD, self::DATABASENAME);
+
+        if (mysqli_connect_errno()) {
+            trigger_error("Problem with connecting to database.");
+        }
+
+        $conn->set_charset("utf8");
+        return $conn;
+    }
+
+   
+    public function getPdoConnection()
+    {
+        $conn = FALSE;
+        try {
+            $dsn = 'mysql:host=' . self::HOST . ';dbname=' . self::DATABASENAME;
+            $conn = new \PDO($dsn, self::USERNAME, self::PASSWORD);
+            $conn->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+        } catch (\Exception $e) {
+            exit("PDO Connect Error: " . $e->getMessage());
+        }
+        return $conn;
+    }
+
+    
+    public function select($query, $paramType = "", $paramArray = array())
+    {
+        $stmt = $this->conn->prepare($query);
+
+        if (! empty($paramType) && ! empty($paramArray)) {
+
+            $this->bindQueryParams($stmt, $paramType, $paramArray);
+        }
+        $stmt->execute();
+        $result = $stmt->get_result();
+
+        if ($result->num_rows > 0) {
+            while ($row = $result->fetch_assoc()) {
+                $resultset[] = $row;
+            }
+        }
+
+        if (! empty($resultset)) {
+            return $resultset;
+        }
+    }
+
+    /**
+     * To insert
+  
+     */
+    public function insert($query, $paramType, $paramArray)
+    {
+        $stmt = $this->conn->prepare($query);
+        $this->bindQueryParams($stmt, $paramType, $paramArray);
+
+        $stmt->execute();
+        $insertId = $stmt->insert_id;
+        return $insertId;
+    }
+
+    /**
+     * To execute query
+   
+     */
+    public function execute($query, $paramType = "", $paramArray = array())
+    {
+        $stmt = $this->conn->prepare($query);
+
+        if (! empty($paramType) && ! empty($paramArray)) {
+            $this->bindQueryParams($stmt, $paramType, $paramArray);
+        }
+        $stmt->execute();
+    }
+
+ 
+    public function bindQueryParams($stmt, $paramType, $paramArray = array())
+    {
+        $paramValueReference[] = & $paramType;
+        for ($i = 0; $i < count($paramArray); $i ++) {
+            $paramValueReference[] = & $paramArray[$i];
+        }
+        call_user_func_array(array(
+            $stmt,
+            'bind_param'
+        ), $paramValueReference);
+    }
+
+    /**
+     * To get database results
+  
+     */
+    public function getRecordCount($query, $paramType = "", $paramArray = array())
+    {
+        $stmt = $this->conn->prepare($query);
+        if (! empty($paramType) && ! empty($paramArray)) {
+
+            $this->bindQueryParams($stmt, $paramType, $paramArray);
+        }
+        $stmt->execute();
+        $stmt->store_result();
+        $recordCount = $stmt->num_rows;
+
+        return $recordCount;
+    }
+}
